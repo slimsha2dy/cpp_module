b8f58186276292cc4d52af11d27960ccc3b36175
monkeyking001 <2018121152@yonsei.ac.kr>
2023-02-19 18:30:50 +0900

    dd

--- 00/ex01/PhoneBook.cpp
+++ 00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: slimsha2dy <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:39:49 by slimsha2          #+#    #+#             */
-/*   Updated: 2023/02/19 12:04:13 by monkeyki         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:29:40 by monkeyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,45 @@ void	PhoneBook::add(int i)
 	std::string	line;
 
 	std::cout << "first name: ";
-	std::getline(std::cin, line);
-	if (!line)
-		std::cout << "NULL\n";
-	if (line == "")
-		std::cout << "empty\n";
-	this->contacts[i].setFirstName(line);
+	while (!std::getline(std::cin, line)) {
+		if (!line.empty()) {
+			this->contacts[i].setFirstName(line);
+			break ;
+		}
+		std::cout << "please enter a non-empty input\n";
+	}
 	std::cout << "last name: ";
-	std::getline(std::cin, line);
-	this->contacts[i].setLastName(line);
+	while (!std::getline(std::cin, line)) {
+		if (!line.empty()) {
+			this->contacts[i].setLastName(line);
+			break ;
+		}
+		std::cout << "please enter a non-empty input\n";
+	}
 	std::cout << "nickname: ";
-	std::getline(std::cin, line);
-	this->contacts[i].setNickName(line);
+	while (!std::getline(std::cin, line)) {
+		if (!line.empty()) {
+			this->contacts[i].setNickName(line);
+			break ;
+		}
+		std::cout << "please enter a non-empty input\n";
+	}
 	std::cout << "phone number: ";
-	std::getline(std::cin, line);
-	this->contacts[i].setPhoneNumber(line);
+	while (!std::getline(std::cin, line)) {
+		if (!line.empty()) {
+			this->contacts[i].setPhoneNumber(line);
+			break ;
+		}
+		std::cout << "please enter a non-empty input\n";
+	}
 	std::cout << "darkest secret: ";
-	std::getline(std::cin, line);
-	this->contacts[i].setSecret(line);
+	while (!std::getline(std::cin, line)) {
+		if (!line.empty()) {
+			this->contacts[i].setSecret(line);
+			break ;
+		}
+		std::cout << "please enter a non-empty input\n";
+	}
 	if (this->size < 8)
 		this->size++;
 }
